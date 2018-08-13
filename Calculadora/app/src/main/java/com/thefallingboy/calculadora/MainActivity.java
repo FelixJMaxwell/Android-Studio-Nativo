c89516feb472d5110bb48bc5ea061a542629dbea
Oliver García Montero <disrupted.night@gmail.com>
2018-08-13 14:55:36 -0500

    Calculadora Terminada

--- Calculadora/app/src/main/java/com/thefallingboy/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/thefallingboy/calculadora/MainActivity.java
@@ -1,14 +1,21 @@
 package com.thefallingboy.calculadora;
 
+import android.net.nsd.NsdManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.res.TypedArrayUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.View;
 import android.widget.TextView;
 
+import java.nio.file.attribute.AclEntryPermission;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvPant;
-
+    private TextView tvPant, tvRes;
     private float resultado = 0;
 
     @Override
@@ -17,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvPant = (TextView)findViewById(R.id.pantalla);
+        tvRes = (TextView)findViewById(R.id.resultado);
 
         DefinirBotones();
     }
@@ -44,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private View.OnClickListener BtnClickListener = new View.OnClickListener() {
+        @RequiresApi(api = Build.VERSION_CODES.N)
         @Override
         public void onClick(View v) {
             switch(v.getId()){
@@ -99,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
                     tvPant.setText(tvPant.getText() + " % ");
                     break;
                 case R.id.buttonIgual:
-
+                    Calcular(tvPant.getText().toString());
                     break;
                 case R.id.buttonPunto:
                     tvPant.setText(tvPant.getText()+ ".");
@@ -107,4 +116,121 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public float Calcular(String _Operacion){
+        String[] partes = _Operacion.split(" ");
+        int suma = 0, resta = 0, mult = 0, div = 0;
+        float operando1 = 0, operando2 = 0;
+        float resultadoOperacion = 0;
+        for (int i = 0; i < partes.length; i++){
+            if (partes[i] != "+"){
+                suma++;
+            }
+            if (partes[i] != "-"){
+                resta++;
+            }
+            if (partes[i] != "*"){
+                mult++;
+            }
+            if (partes[i] != "/"){
+                div++;
+            }
+
+            if (div >= 1){
+                for (int e = 0; e < div; e++){
+                    for (int j = 0; j < partes.length; j++){
+                        if (partes[j] != null && partes[j].equals("/")){
+                            operando1 = Float.parseFloat(partes[j-1]);
+                            operando2 = Float.parseFloat(partes[j+1]);
+                            resultadoOperacion = operando1/operando2;
+                            partes[j-1] = String.valueOf(resultadoOperacion);
+                            partes[j+1] = null;
+                            partes[j] = null;
+                        }
+
+                        partes = Arrays.stream(partes)
+                                .filter(s -> (s != null && s.length() > 0))
+                                .toArray(String[]::new);
+                    }
+                }
+
+                for (int m = 0; m < partes.length; m++){
+                    //System.out.print(partes[m] + " ");
+                    //tvPant.setText(tvPant.getText() + partes[m]);
+                    tvRes.setText(partes[m]);
+                }
+            }
+            if (mult >= 1){
+                for (int e = 0; e < mult; e++){
+                    for (int j = 0; j< partes.length; j++){
+                        if (partes[j] != null && partes[j].equals("*")){
+                            operando1 = Float.parseFloat(partes[j-1]);
+                            operando2 = Float.parseFloat(partes[j+1]);
+                            resultadoOperacion = operando1 * operando2;
+                            partes[j-1] = String.valueOf(resultadoOperacion);
+                            partes[j+1] = null;
+                            partes[j] = null;
+                        }
+
+                        partes = Arrays.stream(partes)
+                                .filter(s -> (s != null && s.length() > 0))
+                                .toArray(String[]::new);
+                    }
+                }
+                for (int m = 0; m < partes.length; m++){
+                    //System.out.print(partes[m] + " ");
+                    //tvPant.setText(tvPant.getText() + partes[m]);
+                    tvRes.setText(partes[m]);
+                }
+            }
+            if (resta >= 1){
+                for (int e = 0; e < resta; e++){
+                    for (int j = 0; j < partes.length; j++){
+                        if (partes[j] != null && partes[j].equals("-")) {
+                            operando1 = Float.parseFloat(partes[j-1]);
+                            operando2 = Float.parseFloat(partes[j+1]);
+                            resultadoOperacion = operando1 - operando2;
+                            partes[j-1] = String.valueOf(resultadoOperacion);
+                            partes[j+1] = null;
+                            partes[j] = null;
+                        }
+
+                        partes = Arrays.stream(partes)
+                                .filter(s -> (s != null && s.length() > 0))
+                                .toArray(String[]::new);
+                    }
+                }
+                for (int m = 0; m < partes.length; m++){
+                    //System.out.print(partes[m] + " ");
+                    //tvPant.setText(tvPant.getText() + partes[m]);
+                    tvRes.setText(partes[m]);
+                }
+            }
+            if (suma >= 1){
+                for (int e = 0; e < suma; e++){
+                    for (int j = 0; j < partes.length; j++){
+                        if (partes[j] != null && partes[j].equals("+")){
+                            operando1 = Float.parseFloat(partes[j-1]);
+                            operando2 = Float.parseFloat(partes[j+1]);
+                            resultadoOperacion = operando1 + operando2;
+                            partes[j-1] = String.valueOf(resultadoOperacion);
+                            partes[j+1] = null;
+                            partes[j] = null;
+                        }
+
+                        partes = Arrays.stream(partes)
+                                .filter(s -> (s != null && s.length() > 0))
+                                .toArray(String[]::new);
+                    }
+                }
+                for (int m = 0; m < partes.length; m++){
+                    //System.out.print(partes[m] + " ");
+                    //tvPant.setText(tvPant.getText() + partes[m]);
+                    tvRes.setText(partes[m]);
+                }
+            }
+        }
+        return resultado;
+    }
 }
